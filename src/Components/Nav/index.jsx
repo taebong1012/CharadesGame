6b806d27ae313c1f79e6204aef3a42f6d2ddae83
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 03:57:08 +0900

    :sparkles: Nav 바 구현

---
+++ src/Components/Nav/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ReactComponent as PrevIco } from "../../Sources/svg/prev.svg";
+import { Frame, IconDiv } from "./style";
+import { useNavigate } from "react-router-dom";
+
+const Nav = () => {
+  const navigate = useNavigate();
+
+  const goMain = () => {
+    navigate("/");
+  };
+
+  return (
+    <Frame>
+      <IconDiv>
+        <PrevIco width={"80%"} height={"60%"} onClick={goMain} />
+      </IconDiv>
+    </Frame>
+  );
+};
+
+export default Nav;
---
+++ src/Components/Nav/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Frame = styled.div`
+  height: 2.625rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+`;
+
+export const IconDiv = styled.div`
+  height: 1.875rem;
+  width: 1.875rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
