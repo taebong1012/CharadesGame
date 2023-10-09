bf4fc2736422aad171c07dc891cb60e3d7d191cb
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 04:44:25 +0900

    :sparkles: 시간 설정 페이지 구현

---
+++ src/Screen/setGame/setTime/index.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import {
+  BlankWidth,
+  Container,
+  Content,
+  TimeArea,
+  Text,
+  TimeSelect,
+  Btn,
+} from "./style";
+import Nav from "../../../Components/Nav";
+import Title from "../../../Components/Title";
+import { useNavigate } from "react-router-dom";
+
+const SetTime = () => {
+  const [selectedMin, setSelectedMin] = useState(1);
+  const [selectedSec, setSelectedSec] = useState(0);
+
+  useEffect(() => {
+    const min = JSON.parse(sessionStorage.getItem("min"));
+    const sec = JSON.parse(sessionStorage.getItem("sec"));
+    if (min && sec) {
+    }
+  }, []);
+
+  const handleChangeMin = (e) => {
+    setSelectedMin(e.target.value);
+  };
+
+  const handleChangeSec = (e) => {
+    setSelectedSec(e.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const handleClickNext = () => {
+    sessionStorage.setItem("min", JSON.stringify(selectedMin));
+    sessionStorage.setItem("sec", JSON.stringify(selectedSec));
+    navigate("/ingame");
+  };
+
+  return (
+    <Container>
+      <Nav />
+      <Content>
+        <Title
+          title="시간 설정"
+          caption="얼만큼의 시간동안 게임을 진행할지 선택하세요."
+        />
+        <TimeArea>
+          <TimeSelect onChange={handleChangeMin}>
+            {Array.from({ length: 10 }, (_, index) => (
+              <option key={index + 1} value={index + 1}>
+                {index + 1}
+              </option>
+            ))}
+          </TimeSelect>
+          <Text>분</Text>
+          <BlankWidth />
+          <TimeSelect onChange={handleChangeSec}>
+            {Array.from({ length: 6 }, (_, index) => (
+              <option key={index} value={10 * index}>
+                {10 * index}
+              </option>
+            ))}
+          </TimeSelect>
+          <Text>초</Text>
+        </TimeArea>
+        <Btn onClick={handleClickNext}>다음</Btn>
+      </Content>
+    </Container>
+  );
+};
+
+export default SetTime;
---
+++ src/Screen/setGame/setTime/style.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+`;
+
+export const Content = styled.div`
+  padding: 0 1.25rem;
+`;
+
+export const BlankWidth = styled.div`
+  width: 10px;
+`;
+
+export const TimeSelect = styled.select`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  border: none;
+  background-color: #f2f3f5;
+  border-radius: 10px;
+  padding: 4px;
+  font-size: 1rem;
+  font-family: "LINE400";
+`;
+
+export const Text = styled.div`
+  margin: 0 0.5rem;
+  font-size: 1.2rem;
+`;
+
+export const TimeArea = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 18.75rem;
+`;
+
+export const Btn = styled.button`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  background-color: #ff8a00;
+  color: white;
+  height: 3rem;
+  width: calc(100% - 2.5rem);
+  border: none;
+  border-radius: 10px;
+  position: fixed;
+  bottom: 0%;
+`;
