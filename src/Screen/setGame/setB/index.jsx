537b7fd713976d3891241ba1d4575cfd5f0ca909
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 02:58:42 +0900

    :sparkles: B팀 단어 입력 페이지 구현

---
+++ src/Screen/setGame/setB/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Title } from "./style";
+import InputForm from "../../../Components/inputForm";
+
+const Setb = () => {
+  return (
+    <>
+      <Title>B팀 작성 단어</Title>
+      <InputForm team="b" />
+    </>
+  );
+};
+
+export default Setb;
