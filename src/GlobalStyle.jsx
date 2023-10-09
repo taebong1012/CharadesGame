7212a5f98ecea1226ae9f0e3b4ea4facd9cbb09d
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 00:01:38 +0900

    :lipstick: 폰트 설정

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -5,8 +5,15 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  @font-face {
+    font-family: 'LINESeedKR-Bd';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+}
+
   body {
-    font-family: "Helvetica", "Arial", sans-serif;
+    font-family: "LINESeedKR-Bd", sans-serif;
     line-height: 1.5;
   }
 `;
