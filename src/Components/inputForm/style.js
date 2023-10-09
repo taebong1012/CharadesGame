557bb6363a8be230b77ee3b4561129ccc281a114
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 02:48:27 +0900

    :lipstick: input 컴포넌트 스타일 적용

---
+++ src/Components/inputForm/style.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+  font-family: "LINE700";
+  margin: 2.5rem 0;
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InputRow = styled.div`
+  display: flex;
+  margin: 8px 0;
+`;
+
+export const Num = styled.div`
+  margin-right: 0.5rem;
+  width: 20px;
+  height: 2rem;
+  font-size: 1.125rem;
+  display: flex;
+  align-items: end;
+  justify-content: end;
+  font-family: "LINE400";
+`;
+
+export const Input = styled.input`
+  height: 3rem;
+  width: 14rem;
+  font-size: 1.125rem;
+  font-family: "LINE100";
+  padding: 0.625rem;
+  border: none;
+  background-color: #f2f3f5;
+  border-radius: 10px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const SubmitBtn = styled.button`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  background-color: ${(props) => (props.disabled ? "#ADB3BA" : "#ff8a00")};
+  color: white;
+  height: 3rem;
+  border: none;
+  border-radius: 10px;
+`;
