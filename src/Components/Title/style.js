8437a00f1d6aa08e151daf93acfe94d3f497ae0a
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 05:11:37 +0900

    :bug: 태그명 수정

--- src/Components/Title/style.js
+++ src/Components/Title/style.js
@@ -5,13 +5,13 @@ export const Container = styled.div`
   flex-direction: column;
 `;
 
-export const TitleText = styled.text`
+export const TitleText = styled.p`
   font-size: 1.625rem;
   font-family: "LINE700";
   margin-bottom: 0.25rem;
 `;
 
-export const CaptionText = styled.text`
+export const CaptionText = styled.p`
   font-size: 0.75rem;
   font-family: "LINE400";
   color: #888e93;
