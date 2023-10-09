77d9ec05e7eb2e92be4d3e29936bdf391d66d886
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 00:08:33 +0900

    :lipstick: 폰트 굵기 종류 추가

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -6,14 +6,28 @@ const GlobalStyle = createGlobalStyle`
   }
 
   @font-face {
-    font-family: 'LINESeedKR-Bd';
+    font-family: 'LINE100';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+    font-weight: 100;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'LINE400';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'LINE700';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
     font-weight: 700;
     font-style: normal;
 }
 
   body {
-    font-family: "LINESeedKR-Bd", sans-serif;
+    font-family: "LINE100", sans-serif;
     line-height: 1.5;
   }
 `;
