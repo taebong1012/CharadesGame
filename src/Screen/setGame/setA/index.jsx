639548ede66005bc5c274808dffab951c72860be
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 02:57:06 +0900

    :sparkles: A팀 단어 입력 페이지 구현

---
+++ src/Screen/setGame/setA/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Title } from "./style";
+import InputForm from "../../../Components/inputForm";
+
+const Seta = () => {
+  return (
+    <>
+      <Title>A팀 작성 단어</Title>
+      <InputForm team="a" />
+    </>
+  );
+};
+
+export default Seta;
---
+++ src/Screen/setGame/setA/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+  font-family: "LINE700";
+  margin: 2.5rem 0;
+  font-size: 1.5rem;
+  text-align: center;
+`;
