6ae8b1b7d28c6123a6ae56daa3904cb4f4b3b8e0
taebong1012 <dksxogus1012@gmail.com>
2023-10-10 10:58:32 +0900

    :sparkles: 게임 결과 페이지 구현

--- src/Screen/Result/index.jsx
+++ src/Screen/Result/index.jsx
@@ -1,7 +1,131 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Nav from "../../Components/Nav";
+import {
+  Container,
+  Content,
+  WinLoseGroup,
+  Semititle,
+  WinLose,
+  Line,
+  TeamName,
+  Info,
+  Answer,
+  Correct,
+  Num,
+} from "./style";
+import Title from "../../Components/Title";
 
 const Result = () => {
-  return <></>;
+  const aAnswer = JSON.parse(sessionStorage.getItem("aanswer"));
+  const bAnswer = JSON.parse(sessionStorage.getItem("banswer"));
+  const aResult = JSON.parse(sessionStorage.getItem("aresult"));
+  const bResult = JSON.parse(sessionStorage.getItem("bresult"));
+  const [aScore, setaScore] = useState(0);
+  const [bScore, setbScore] = useState(0);
+  const [isDraw, setIsDraw] = useState(false);
+  const [winner, setWinner] = useState("A");
+  const [loser, setLoser] = useState("B");
+  const [isShowWinner, setisShowWinner] = useState(true);
+
+  useEffect(() => {
+    let cnt = 0;
+    aResult.forEach((element) => {
+      if (element === "O") cnt++;
+    });
+    setaScore(cnt);
+
+    cnt = 0;
+    bResult.forEach((element) => {
+      if (element === "O") cnt++;
+    });
+    setbScore(cnt);
+  }, []);
+
+  useEffect(() => {
+    if (aScore === bScore) {
+      setIsDraw(true);
+    } else if (bScore > aScore) {
+      setWinner("B");
+      setLoser("A");
+      setIsDraw(false);
+    } else {
+      setWinner("A");
+      setLoser("B");
+      setIsDraw(false);
+    }
+  }, [aScore, bScore]);
+
+  const clickWinner = () => {
+    setisShowWinner(true);
+  };
+
+  const clickLoser = () => {
+    setisShowWinner(false);
+  };
+
+  const [showAnswer, setShowAnswer] = useState([]);
+  const [showResult, setShowResult] = useState([]);
+
+  useEffect(() => {
+    if ((isShowWinner && winner == "A") || (!isShowWinner && winner == "B")) {
+      setShowAnswer(aAnswer);
+      setShowResult(aResult);
+    } else if (
+      (isShowWinner && winner == "B") ||
+      (!isShowWinner && winner == "A")
+    ) {
+      setShowAnswer(bAnswer);
+      setShowResult(bResult);
+    }
+  }, [isShowWinner, winner]);
+
+  const Answers = () => {
+    const infoElements = [];
+
+    for (let i = 0; i < 20; i++) {
+      if (showAnswer[i]) {
+        infoElements.push(
+          <Info key={i}>
+            <Num>{i + 1}</Num>
+            <Answer>{showAnswer[i]}</Answer>
+            <Correct>{showResult[i]}</Correct>
+          </Info>
+        );
+      }
+    }
+
+    return infoElements;
+  };
+
+  return (
+    <Container>
+      <Nav />
+      <Content>
+        <Title
+          title="게임 결과"
+          caption="팀을 누르면 정답들을 확인할 수 있어요."
+        />
+        <WinLoseGroup>
+          <WinLose onClick={clickWinner} isShowWinner={isShowWinner}>
+            <Semititle>{isDraw ? "DRAW" : "WIN"}</Semititle>
+            <TeamName>{winner}</TeamName>
+          </WinLose>
+          <WinLose onClick={clickLoser} isShowWinner={!isShowWinner}>
+            <Semititle>{isDraw ? "DRAW" : "LOSE"}</Semititle>
+            <TeamName>{loser}</TeamName>
+          </WinLose>
+        </WinLoseGroup>
+
+        <Info title={1}>
+          <Num>번호</Num>
+          <Answer>정답</Answer>
+          <Correct>O/X</Correct>
+        </Info>
+        <Line />
+        <Answers />
+      </Content>
+    </Container>
+  );
 };
 
 export default Result;
--- src/Screen/Result/style.js
+++ src/Screen/Result/style.js
@@ -1,1 +1,83 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+`;
+
+export const Content = styled.div`
+  padding: 0 1.875rem;
+`;
+
+export const WinLoseGroup = styled.div`
+  margin-top: 10px;
+  display: flex;
+  width: 100%;
+  height: 80px;
+  margin-bottom: 20px;
+`;
+
+export const WinLose = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: calc((100% - 8px) / 2);
+  margin: 0 2px;
+  border-radius: 10px;
+  background-color: ${(props) => (props.isShowWinner ? "#FEF0EE" : "none")};
+`;
+
+export const Semititle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 40%;
+`;
+
+export const TeamName = styled.div`
+  font-family: "LINE700";
+  font-size: 1.8rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 60%;
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  border: 0.5px solid #adb3ba;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin: 10px 0;
+  font-family: ${(props) => (props.title ? "LINE700" : "LINE100")};
+`;
+
+export const Num = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20%;
+`;
+
+export const Answer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 60%;
+  font-family: "LINE400";
+`;
+
+export const Correct = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20%;
+  font-family: "LINE400";
+`;
